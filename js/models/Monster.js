e010e1f058623638be38b7f180109b42004bfc75
Rick van den Heuvel <Rick.heuvel@outlook.com>
2019-07-17 15:43:33 +0200

    created monster model

---
+++ js/models/Monster.js
@@ -0,0 +1,24 @@
+class Monster {
+
+    constructor(id, name, type, strength, amountOfArms, typeOfArms, amountOfLegs, amountOfEyes, furType, colour, canFly, canSwim, specialPower){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.strength = strength;
+        this.amountOfArms = amountOfArms;
+        this.typeOfArms = typeOfArms;
+        this.amountOfLegs = amountOfLegs;
+        this.amountOfEyes = amountOfEyes;
+        this.furType = furType;
+        this.colour = colour;
+        this.canFly = canFly;
+        this.canSwim = canSwim;
+        this.specialPower = specialPower;
+        this.xPosition;
+        this.yPosition;
+        this.region;
+    }
+
+
+
+}
