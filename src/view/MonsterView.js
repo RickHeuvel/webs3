cca8c41b321e4ac7a31868dd909f8b5a7a852855
Rick <ricksieben9@hotmail.com>
2019-07-17 20:07:07 +0200

    set up todo list in MonsterView
    fixed typo's:
    bulbasaur.jpg -> earth.jpg
    index.html

--- src/view/MonsterView.js
+++ src/view/MonsterView.js
@@ -12,6 +12,38 @@ class MonsterView
 
     setUpMonsterConfig()
     {
+        let monsterConfiguratorWrapper = document.querySelector(".monster-configurator-wrapper");
+        let monsterConfigurator = document.querySelector(".monster-configurator");
 
+        // set title (h1)
+
+        // create form
+
+        // create id holder
+
+        // create name holder
+
+        // create type holder
+
+        // create strength holder
+
+        // create amountOfArms holder
+
+        // create typeOfArms holder
+
+        // create amountOfLegs holder
+
+        // create amountOfEyes holder
+
+        // create furType holder
+
+        // create colour holder
+
+        // create canFly holder
+
+        // create canSwim holder
+
+        // create specialPower holder
+        
     }
 }
