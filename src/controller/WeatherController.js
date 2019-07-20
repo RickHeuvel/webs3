d46e450fb333653d6bd3b882240172713003e8d1
Rick <ricksieben9@hotmail.com>
2019-07-20 14:06:20 +0200

    started setting up weather box with functionality
    and changed styling a bit

--- src/controller/WeatherController.js
+++ src/controller/WeatherController.js
@@ -9,4 +9,15 @@ class WeatherController
     {
         this.weatherView.setUpWeatherView();
     }
+
+    setWeather(location)
+    {
+        const API_KEY = 'd6a207bba10f6e36888b5bf90ba68cb7';
+        const LOCATION = location;
+        const COUNTRY = 'nl';
+        const URL = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION},${COUNTRY}&APPID=${API_KEY}`;
+        var self = this;
+
+
+    }
 }
--- src/view/WeatherView.js
+++ src/view/WeatherView.js
@@ -25,6 +25,79 @@ class WeatherView
 
     createWeatherBox()
     {
+        let weatherBox = document.querySelector(".weather-box");
+        let searchBoxHolder = document.createElement("div");
+        searchBoxHolder.classList.add("search-box-container");
+        let weatherForm = document.createElement("div");
+        let searchLabel = document.createElement("div");
+        let searchLabelContent = document.createTextNode("Search location");
+        let searchBox = document.createElement("input");
+        searchBox.id = 'search-box';
+        searchBox.type = 'text';
+        searchBox.placeholder = 'Location..';
+        searchBox.name = 'search-box';
+        let searchButton = document.createElement("button");
+        searchButton.id = 'search-button';
+        searchButton.type = 'submit';
+        searchButton.textContent = 'Search';
+        let hr = document.createElement("hr");
+
+        // callback
+        let searchButtonCallback = this.checkWeather.bind(this);
+        let searchBoxCallback = this.checkWeather.bind(this);
+
+        searchButton.addEventListener("click", searchButtonCallback);
+        searchBox.addEventListener("enter", searchBoxCallback);
+
+        searchBoxHolder.appendChild(weatherForm);
+        weatherForm.appendChild(searchBox);
+        weatherForm.appendChild(searchButton);
+        searchLabel.appendChild(searchLabelContent);
+        weatherBox.appendChild(searchLabel);
+        weatherBox.appendChild(searchBoxHolder);
+        weatherBox.appendChild(hr);
+
+        let weatherDataDiv = document.createElement("div");
+        weatherDataDiv.id = "weather-data";
+        weatherBox.appendChild(weatherDataDiv);
+
+        // special attack gif box
+        let weatherTitle = document.querySelector(".weather-title");
+        let specialAttackDiv = document.createElement("div");
+        specialAttackDiv.classList.add("gif-holder");
+        weatherTitle.appendChild(specialAttackDiv);
 
     }
+
+    EnterListener(event)
+    {
+        if(event.key === "Enter")
+        {
+            console.log("waaa");
+            this.checkWeather();
+        }
+    }
+
+    checkWeather()
+    {
+        console.log("checkeee");
+        let searchBox = document.querySelector("#search-box");
+
+        if(searchBox.value !== "")
+        {
+            this.weatherController.setWeather(searchBox.value);
+        }
+    }
+
+    // setUpLocation(locationText)
+    // {
+    //     let locationHolder = document.createElement("div");
+    //     locationHolder.classList.add("location-container");
+    // }
+
+    showWeather(weather)
+    {
+
+    }
+
 }
