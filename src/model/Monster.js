6ab98794102812f3ff0e3781ac2f9c17c3920f99
Rick van den Heuvel <Rick.heuvel@outlook.com>
2019-08-18 13:05:27 +0200

    removed all special attack stuff

--- src/model/Monster.js
+++ src/model/Monster.js
@@ -1,6 +1,6 @@
 class Monster {
 
-    constructor(id, name, type, strength, amountOfArms, typeOfArms, amountOfLegs, amountOfEyes, furType, colour, canFly, canSwim, specialPower){
+    constructor(id, name, type, strength, amountOfArms, typeOfArms, amountOfLegs, amountOfEyes, furType, colour, canFly, canSwim){
         this.id = id;
         this.name = name;
         this.type = type;
@@ -14,7 +14,6 @@ class Monster {
         this.colour = colour;
         this.canFly = canFly;
         this.canSwim = canSwim;
-        this.specialPower = specialPower;
         this.xPosition;
         this.yPosition;
         this.region;
